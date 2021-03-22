9d9ade81b1fbf57faceccd6526e91f4ae92f4d1b
snsvrno <snsvrno@tuta.io>
2021-03-22 16:56:50 -0600

    test: creates first test
    creates a basic test that will run a simple project
    and check if the library version is changed.

---
+++ tests/Test.hx
@@ -0,0 +1,35 @@
+import sys.io.Process;
+import utest.Assert;
+import utest.Async;
+
+class Test extends utest.Test {
+
+	static public function main() utest.UTest.run([new Test()]);
+
+	/**
+	 * Testing if it will correctly identify the current haxelib version
+	 * and change it to the desired haxelib version when trying to build
+	 * a project.
+	 */
+	public function testProject1() {
+
+		// ensures we are in the test project folder.
+		Sys.setCwd("tests/project1");
+
+		// the setup, changing the library version to the "older" version.
+		var process = new sys.io.Process("haxelib", ["set", "heaps", "1.9.0", "--always"]);
+		process.close();
+
+		// checks that the library is the version we want.
+		Assert.equals("1.9.0", apps.Haxelib.getLibrary("heaps").version);
+
+		// running the build, equivalent to the user running a build with haxelock.
+		var build = new commands.Build();
+		build.run("build.hxml",[]);
+
+		// checks to see if what expected happened, updating the version of 'heaps'
+		// to the one listed in the lock file.
+		Assert.equals("1.9.1", apps.Haxelib.getLibrary("heaps").version);		
+	}
+
+}
---
+++ tests/project1/Main.hx
@@ -0,0 +1,3 @@
+class Main {
+	static public function main() { }
+}
